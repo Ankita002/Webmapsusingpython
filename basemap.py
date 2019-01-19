f08167a5050f480af4f50c3ea244f860feb1b2c9
Ankita Rafiz <sabitarafiz@gmail.com>
2019-01-19 10:43:23 +0530

    Add files via upload

---
+++ basemap.py
@@ -0,0 +1,7 @@
+import folium
+​map=folium.Map(location=[80,-100])
+map.save("Map1.html")
+map= folium.Map(location=[38.58,-99.09])
+map.save("Map1.html")
+map= folium.Map(location=[38.58, -99.09],zoom_start=6)
+map.save("Map1.html")
